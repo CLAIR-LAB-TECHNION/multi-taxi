cb34add40100a7be6f69ba8b64cdaf9521b7ceec
Guy Azran <guy.azran@campus.technion.ac.il>
2023-03-14 09:10:26 +0200

    release 0.3.0: Objective control and strict passenger ordering
    - agent timeout default reward set to 0.
    - new `OBJECTIVE` event for objective completion.
    - new configuration for toggling agent ability to perform an intermediate dropoff (not at destination).
    - new configuration for strict ordering of passenger pickup and dropoff.
    - general bugfixes and cleanups

--- multi_taxi/__init__.py
+++ multi_taxi/__init__.py
@@ -3,4 +3,4 @@ from .env import multi_taxi_v0, single_taxi_v0, config
 from .utils.types import FuelType, ObservationType, Event, Action
 from .world import maps
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
